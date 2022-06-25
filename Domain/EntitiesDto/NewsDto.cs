997fc76d356fff856b5f1d8de2f12e0f6d2acdda
ZohirH <105782897+ZohirH@users.noreply.github.com>
2022-06-25 14:42:50 +0500

    DTOs
    AddDto

--- Domain/EntitiesDto/NewsDto.cs
+++ Domain/EntitiesDto/NewsDto.cs
@@ -8,5 +8,12 @@ namespace Domain.EntitiesDto
 {
     public class NewsDto
     {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool Enabled { get; set; }
+
     }
 }
--- Domain/EntitiesDto/StudentDto.cs
+++ Domain/EntitiesDto/StudentDto.cs
@@ -8,5 +8,15 @@ namespace Domain.EntitiesDto
 {
     public class StudentDto
     {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
+        public string? Title { get; set; }
+        public string? Address { get; set; }
+        public bool Enabled { get; set; }
+        public string? PhoneNumber { get; set; }
+        public int Age { get; set; }
     }
 }
